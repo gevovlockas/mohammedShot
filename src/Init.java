aff8827a36d5c91f54343d83fa7fcbef9b35a29b
Henry <katiun7@hotmail.com>
2014-10-17 22:55:37 -0200

    Cambio comunicación.
    Se comunican a través de la función read de la conexión. Se comenta
    Thread.

--- src/Init.java
+++ src/Init.java
@@ -13,6 +13,7 @@ import lejos.util.Delay;
 public class Init {
 
 	
+	private static RS485Connection connection;
 	/* 
 	 * 0 - Dejar de disparar 
 	 * 1 - Disparo bajo
@@ -21,32 +22,71 @@ public class Init {
 	 * 
 	 * */
 	public static void main(String[] args) {
+		
+		byte[] readValue = {0};
         int value = 0;
         ShotBall shot = new ShotBall();
 		LCD.clear();
 		
-		DataInputStream dis = null;
-		RS485Connection connection = RS485.waitForConnection(0, NXTConnection.PACKET);
+//		DataInputStream dis = null;
+		connect();
+//		dis = connection.openDataInputStream();
+//        Thread t = new Thread(shot);
+//        t.start();
+        while (true){
+
+	        try{
+//	            System.out.println("Esperando valor");
+//	        	value = dis.readInt();
+//	        	shot.setValue(value);
+//	            System.out.println("Value = " + value);
+	        	if (connection == null){
+	        		connect();
+	        	}
+	        	value = 1;
+	        	connection.read(readValue, value, true);
+	        	if (value < 0){
+	        		System.out.println("Error al leer");
+	        		Delay.msDelay(3000);
+	        	}else{
+		        	System.out.println("Read");
+		        	value = byteArrayToInt(readValue);
+//		        	shot.setValue(value);
+		            System.out.println("Value = " + value);
+		            Delay.msDelay(2000);
+	        	}
+	        }catch (Exception ioe){
+	            System.out.println("Error al recibir datos");
+	            try{
+	            	connection.close();
+	            }finally{
+	            	connection = null;
+	            }
+	        }
+
+        }
+	}
+
+	private static void connect(){
+		System.out.println("Aceptando conexion");
+		connection = RS485.waitForConnection(0, NXTConnection.PACKET);
 		if (connection == null){
 			System.out.println("Error al conectar");
 			Delay.msDelay(5000);
 			System.exit(1);
 		}
-		dis = connection.openDataInputStream();
-        Thread t = new Thread(shot);
-        t.start();
-        while (true){
-
-	        try{
-	            System.out.println("Esperando valor");
-	        	value = dis.readInt();
-	        	shot.setValue(value);
-	            System.out.println("Value = " + value);
-	        }catch (IOException ioe){
-	            System.out.println("Error al recivir datos");
-	        }
-
-        }
 	}
 	
+	public static int byteArrayToInt(byte[] b){
+	    int value = 0;
+	    try{
+		    for (int i = 0; i < b.length; i++) {
+		        int shift = (b.length - 1 - i) * 8;
+		        value += (b[i] & 0x000000FF) << shift;
+		    }
+	    }catch(Exception ex){
+	    	System.out.println(ex.getMessage());
+	    }
+	    return value;
+	}
 }
