f186f24330050e2dcb3f33cd8f07b4da207ae7a8
Henry <katiun7@hotmail.com>
2014-11-21 21:30:54 -0200

    Cambio Angulo Sensor

--- src/Constants.java
+++ src/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
 	public final static NXTRegulatedMotor SENSOR_MOTOR = Motor.B;
 	
 	public final static int SPEED_SENSOR_MOTOR = 150;
-	public final static int ANGLE_SENSOR_MOTOR = 115;
+	public final static int ANGLE_SENSOR_MOTOR = 90;
 }
